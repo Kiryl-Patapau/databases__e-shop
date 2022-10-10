d7300e44e3da564200a84d20cd552143f39bb7ec
Kiryl Patapau <Kiryl_Patapau@epam.com>
2022-10-10 16:30:03 +0300

    Added order DTOs

---
+++ src/BlazorShared/Models/OrderDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorShared.Models;
+
+public class OrderDto
+{
+    public string ShippingAddress { get; set; }
+
+    public IEnumerable<OrderItemDto> Items { get; set; }
+
+    public decimal FinalPrice { get; set; }
+}
